0a541d4081aeeaadf526da8b23a356e6829b56d5
elaineo <elaine.ou@gmail.com>
2018-10-05 20:36:41 -0700

    screenshot: crop big pages

--- screenshot.py
+++ screenshot.py
@@ -4,26 +4,25 @@ from bs4 import BeautifulSoup
 import sys, logging, traceback
 
 def screenshot(twit_url, twit_id):
+	options = {
+	    'encoding': "UTF-8",
+	    'javascript-delay': '1000',
+	    'crop-h': 600,
+	    'xvfb': ''
+	}
+	imgfile = twit_id + '.png'
+	
 	try:
 		f = urllib.request.urlopen(twit_url) 
 		page = BeautifulSoup(f, 'html.parser')
 		tweet = page.find("div", class_="permalink-tweet")
-		if not tweet:
-			target = str(page)
-		else: 
+		if tweet:
 			head = page.head
 			css_hack = BeautifulSoup("<style>.icon { background: transparent }</style>", "html.parser")
 			head.append(css_hack)
-			target = str(head) + str(tweet)
-
-		options = {
-		    'encoding': "UTF-8",
-		    'javascript-delay': '1000',
-		    "xvfb": ""
-		}
-
-		imgfile = twit_id + '.png'
-		imgkit.from_string(target, imgfile, options=options)
+			imgkit.from_string(str(head) + str(tweet), imgfile, options=options)
+		else:
+			imgkit.from_url(twit_url, imgfile, options=options)
 	except Exception as e:
 		logging.debug(traceback.format_exception(*sys.exc_info()))
 		return str(e)
